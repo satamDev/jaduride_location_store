fd04dcaea6599a8ad1e0e7b8a2136327a3373129
Satam kundu <satam@v-xplore.com>
2023-08-08 10:19:50 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+const PORT = 3000;
+let driverData = {};
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    let driverId = req.query.driver_id
+    let lat = req.query.lat
+    let lng = req.query.lng
+
+    driverData[driverId] = [lat, lng]
+    res.status(200).json(true);
+})
+
+app.get('/getAllDrivers', (req, res) => {
+    res.status(200).json(driverData)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
